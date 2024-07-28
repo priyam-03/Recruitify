f89e49b1e28a7c54151be018aa5bbd0490c6ebf0
Priyam <priyam.saha2003@gmail.com>
2024-07-28 20:42:40 +0530

    pushed

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 connectDatabase();
 console.log(process.env.PORT);
 
-server.listen(process.env.port, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:4000`);
 });
 
