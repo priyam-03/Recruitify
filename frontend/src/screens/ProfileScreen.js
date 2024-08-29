57647c5681f1574aef957799733202ff0a048a64
Priyam <priyam.saha2003@gmail.com>
2024-08-30 00:27:27 +0530

    some changes

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -8,12 +8,12 @@ import EducationForm from "../User/educations";
 import ExperienceForm from "../User/experience";
 import SkillForm from "../User/skillsForm";
 import LinkForm from "../User/linkForm";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import { fetchResume, uploadResume } from "../store/slices/profileSlices";
 
 const ProfileScreen = () => {
-  const [activeSection, setActiveSection] = useState('education');
+  const [activeSection, setActiveSection] = useState("education");
   const dispatch = useDispatch();
   const { userInfo } = useSelector((state) => state.auth);
   const [singleFile, setSingleFile] = useState("");
@@ -31,8 +31,7 @@ const ProfileScreen = () => {
   const handleViewResume = async () => {
     try {
       if (resumeUrl) {
-        const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:4000';
-        window.open(`${baseURL}/${resumeUrl}`, '_blank');
+        window.open(resumeUrl, "_blank");
       } else {
         alert("No resume available to view.");
       }
@@ -40,22 +39,22 @@ const ProfileScreen = () => {
       console.error("Error while opening resume:", error);
       alert("An error occurred while fetching your resume.");
     }
-  }
+  };
 
   const handleUploadResume = async (event) => {
     event.preventDefault();
     const selectedFile = event.target.files[0];
 
-    if (selectedFile && selectedFile.type === 'application/pdf') {
+    if (selectedFile && selectedFile.type === "application/pdf") {
       setSingleFile(selectedFile);
 
       try {
         const formData = new FormData();
-        formData.append('file', selectedFile);
+        formData.append("resume", selectedFile);
 
         const response = await dispatch(uploadResume(formData));
 
-        if (response.meta.requestStatus === 'fulfilled') {
+        if (response.meta.requestStatus === "fulfilled") {
           alert("Your resume has been uploaded successfully!");
         } else {
           alert(`Failed to upload your resume: ${selectedFile.name}`);
@@ -75,15 +74,15 @@ const ProfileScreen = () => {
 
   const renderContent = () => {
     switch (activeSection) {
-      case 'posts':
+      case "posts":
         return <Posts />;
-      case 'education':
+      case "education":
         return <EducationForm />;
-      case 'experience':
+      case "experience":
         return <ExperienceForm />;
-      case 'skills':
+      case "skills":
         return <SkillForm />;
-      case 'links':
+      case "links":
         return <LinkForm />;
       default:
         return <div>Posts content here...</div>;
@@ -95,15 +94,19 @@ const ProfileScreen = () => {
       {userInfo && (
         <>
           <div className="profile-left-column">
-            {['posts', 'education', 'experience', 'skills', 'links'].map((section) => (
-              <button
-                key={section}
-                className={`profile-left-column-buttons ${activeSection === section ? 'active' : ''}`}
-                onClick={() => handleButtonClick(section)}
-              >
-                {section.charAt(0).toUpperCase() + section.slice(1)}
-              </button>
-            ))}
+            {["posts", "education", "experience", "skills", "links"].map(
+              (section) => (
+                <button
+                  key={section}
+                  className={`profile-left-column-buttons ${
+                    activeSection === section ? "active" : ""
+                  }`}
+                  onClick={() => handleButtonClick(section)}
+                >
+                  {section.charAt(0).toUpperCase() + section.slice(1)}
+                </button>
+              )
+            )}
           </div>
 
           <div className="profile-middle-column">
@@ -112,7 +115,9 @@ const ProfileScreen = () => {
 
           <div className="profile-right-column">
             <div className="profile-header">
-              <h2 className="profile-welcome">Welcome, {userInfo.user?.name}!</h2>
+              <h2 className="profile-welcome">
+                Welcome, {userInfo.user?.name}!
+              </h2>
               <img
                 className="profile-image"
                 src={`http://localhost:4000/${userInfo.user.avatar.filePath}`}
@@ -123,7 +128,10 @@ const ProfileScreen = () => {
               <p className="profile-email">Email: {userInfo.user?.email}</p>
             </div>
 
-            <Link to="/updatePassword" className="profile-button update-password">
+            <Link
+              to="/updatePassword"
+              className="profile-button update-password"
+            >
               Update Password
             </Link>
 
@@ -131,7 +139,10 @@ const ProfileScreen = () => {
               Update Profile
             </Link>
 
-            <button className="profile-button upload-resume" onClick={handleResumeButtonClick}>
+            <button
+              className="profile-button upload-resume"
+              onClick={handleResumeButtonClick}
+            >
               <div className="resume-section">
                 <CloudUploadIcon />
                 <span>Upload Resume</span>
@@ -140,19 +151,25 @@ const ProfileScreen = () => {
                   accept="application/pdf"
                   onChange={handleUploadResume}
                   ref={fileInputRef}
-                  style={{ display: 'none' }}
+                  style={{ display: "none" }}
                 />
               </div>
             </button>
 
-            <button className="profile-button view-resume" onClick={handleViewResume}>
+            <button
+              className="profile-button view-resume"
+              onClick={handleViewResume}
+            >
               <div className="resume-section">
                 <VisibilityIcon />
                 <span>View Resume</span>
               </div>
             </button>
 
-            <button className="profile-button logout" onClick={() => dispatch(logout())}>
+            <button
+              className="profile-button logout"
+              onClick={() => dispatch(logout())}
+            >
               Logout
             </button>
           </div>
