6c03828e3e69bfc571788bee631054463a136d2f
VBsaini <vbsaini2004@gmail.com>
2024-10-28 20:01:06 +0530

    UI fix - add animations for posts, improve ui for like, dislike and CommentTextsection, add post content border and  add post box shadow

--- frontend/src/shared/components/AllPosts.js
+++ frontend/src/shared/components/AllPosts.js
@@ -1,106 +1,193 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Avatar, Box, IconButton, Typography } from '@mui/material';
-import { Chat as ChatIcon, ThumbUp as ThumbUpIcon, ThumbDown as ThumbDownIcon } from '@mui/icons-material';
-import DropdownMenu from './HamburgerDropdown';
-import { fetchAllPosts } from '../../store/slices/postSlice';
-import '../../styles/posts.css';
-import CommentTextsection from './CommentTextSection';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Avatar, Box, IconButton, Typography } from "@mui/material";
+import {
+  Chat as ChatIcon,
+  ThumbUp as ThumbUpIcon,
+  ThumbDown as ThumbDownIcon,
+} from "@mui/icons-material";
+import DropdownMenu from "./HamburgerDropdown";
+import { fetchAllPosts } from "../../store/slices/postSlice";
+import "../../styles/posts.css";
+import CommentTextsection from "./CommentTextSection";
 
+import { Fade } from "react-awesome-reveal";
+const transitions = {
+  entering: {
+    display: "block",
+  },
+  entered: {
+    opacity: 1,
+    display: "block",
+  },
+  exiting: {
+    opacity: 0,
+    display: "block",
+  },
+  exited: {
+    opacity: "0",
+    display: "none",
+  },
+};
 const AllPost = () => {
-    const dispatch = useDispatch();
-    const allPosts = useSelector((state) => state.posts.allPosts);
-    const [likes, setLikes] = useState({});
-    const [likeMode, setLikeMode] = useState({});
-    const [showTextSection, setShowTextSection] = useState({});
+  const dispatch = useDispatch();
+  const allPosts = useSelector((state) => state.posts.allPosts);
+  const [likes, setLikes] = useState({});
 
-    useEffect(() => {
-        dispatch(fetchAllPosts());
-    }, [dispatch]);
+  const [showTextSection, setShowTextSection] = useState({});
+  const [likeMode, setLikeMode] = useState({});
+  //   to Hold likes and dislikes for a post
+  const [postLikeList, setPostLikeList] = useState({});
+  const [isCommentActive, setIsCommentActive] = useState(false);
 
-    const handleToggleTextSection = (postId) => {
-        setShowTextSection((prevState) => ({
-            ...prevState,
-            [postId]: !prevState[postId],
-        }));
-    };
+  const [transitionState, setTransitionState] = useState(false);
+  useEffect(() => {
+    dispatch(fetchAllPosts());
+  }, [dispatch]);
+  //   useEffect(() => {}, [postLikeList]);
 
-    const handleToggleLike = (postId) => {
-        setLikes((prevState) => {
-            const newLikes = { ...prevState };
-            const isLiked = likeMode[postId];
+  const handleToggleTextSection = (postId) => {
+    setIsCommentActive(!isCommentActive);
+    setShowTextSection((prevState) => ({
+      ...prevState,
+      [postId]: !prevState[postId],
+    }));
+  };
 
-            if (isLiked) {
-                newLikes[postId] = Math.max(0, (newLikes[postId] || 0) - 1);
-            } else {
-                newLikes[postId] = (newLikes[postId] || 0) + 1;
-            }
+  const handleToggleLike = (postId, like = 0) => {
+    setLikes((prevState) => {
+      const newLikes = { ...prevState };
+      const isLiked = likeMode[postId];
+      if (isLiked) {
+        newLikes[postId] = Math.max(0, (newLikes[postId] || 0) - 1);
+      } else {
+        newLikes[postId] = (newLikes[postId] || 0) + 1;
+      }
+      return newLikes;
+    });
+    setPostLikeList((prevState) => {
+      const PostLikes = { ...prevState };
+      if (PostLikes[postId] == like) {
+        PostLikes[postId] = 0;
+      } else {
+        PostLikes[postId] = like;
+      }
+      return PostLikes;
+    });
+    setLikeMode((prevState) => ({
+      ...prevState,
+      [postId]: !prevState[postId],
+    }));
+    console.log(postLikeList);
+  };
 
-            return newLikes;
-        });
-        setLikeMode((prevState) => ({
-            ...prevState,
-            [postId]: !prevState[postId]
-        }));
-    };
+  const { userInfo } = useSelector((state) => state.auth);
 
-    const { userInfo } = useSelector((state) => state.auth);
+  return (
+    <div className="posts-page">
+      <div className="posts">
+        {allPosts &&
+          allPosts.map(
+            (post) =>
+              post && post.author ? ( // Check if post and post.author exist
+                <Fade cascade up fraction={0.2} triggerOnce>
+                  <div key={post._id} className="post">
+                    <div className="post-owner">
+                      <Avatar
+                        src={post.author.avatar.filePath}
+                        alt={post.author.name}
+                        sx={{ width: 56, height: 56 }} // Make sure this is consistent with CSS
+                      />
+                      <div className="username">
+                        <Typography
+                          variant="subtitle1"
+                          className="post-owner-username"
+                        >
+                          {post.author.name}
+                        </Typography>
+                      </div>
+                      <DropdownMenu
+                        postId={post._id}
+                        userEmail={post.author.email}
+                      />
+                    </div>
+                    <div className="post-content">
+                      <Typography variant="body1" className="post-text">
+                        {post.text}
+                      </Typography>
+                    </div>
+                    <Box
+                      sx={{ display: "flex", alignItems: "center", mt: 1 }}
+                      className="post-actions"
+                    >
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          ml: 1,
+                          mt: 1,
+                          mr: 1,
+                          fontSize: 16,
+                          color: "rgb(198, 198, 198)",
+                        }}
+                      >
+                        {likes[post._id] || 0}
+                      </Typography>
+                      <IconButton
+                        onClick={(e) => handleToggleLike(post._id, 1)}
+                        className={
+                          "like-button " +
+                          (postLikeList[post._id] == 1 ? "active" : "")
+                        }
+                      >
+                        <ThumbUpIcon />
+                      </IconButton>
 
-    return (
-        <div className="posts-page">
-            <div className="posts">
-                {allPosts && allPosts.map((post) => (
-                    post && post.author ? ( // Check if post and post.author exist
-                        <div key={post._id} className="post">
-                            <div className="post-owner">
-                                <Avatar
-                                    src={post.author.avatar.filePath}
-                                    alt={post.author.name}
-                                    sx={{ width: 56, height: 56 }} // Make sure this is consistent with CSS
-                                />
-                                <div className="username">
-                                    <Typography variant="subtitle1" className="post-owner-username">{post.author.name}</Typography>
-                                </div>
-                                <DropdownMenu postId={post._id} userEmail={post.author.email} />
-                            </div>
-                            <div className="post-content">
-                                <Typography variant="body1">{post.text}</Typography>
-                            </div>
-                            <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }} className="post-actions">
-                                <IconButton onClick={() => handleToggleLike(post._id)} className="like-button">
-                                    <ThumbUpIcon  />
-                                </IconButton>
-                                <Typography variant="body2" sx={{ ml: 1 }}>
-                                    {likes[post._id] || 0}
-                                </Typography>
-                                <IconButton onClick={() => handleToggleLike(post._id)} className="dislike-button">
-                                    <ThumbDownIcon />
-                                </IconButton>
-                                <IconButton onClick={() => handleToggleTextSection(post._id)} className="comment-button">
-                                    <ChatIcon />
-                                </IconButton>
-                            </Box>
-                            {post.images && post.images.map((image, index) => (
-                                <Box key={index} sx={{ mt: 1 }}>
-                                    <img
-                                        src={image}
-                                        alt="Sample"
-                                        style={{ width: '150px', height: '150px', objectFit: 'cover', borderRadius: '8px' }}
-                                    />
-                                </Box>
-                            ))}
-                            {showTextSection[post._id] && (
-                                <CommentTextsection
-                                    setShowThis={() => handleToggleTextSection(post._id)}
-                                />
-                            )}
-                        </div>
-                    ) : null // Render nothing if post or post.author is missing
-                ))}
-                <Typography variant="body2">No more Posts to show</Typography>
-            </div>
-        </div>
-    );
+                      <IconButton
+                        onClick={() => handleToggleLike(post._id, -1)}
+                        className={
+                          "dislike-button " +
+                          (postLikeList[post._id] == -1 ? "active" : "")
+                        }
+                      >
+                        <ThumbDownIcon />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => handleToggleTextSection(post._id)}
+                        className={
+                          "comment-button " + (isCommentActive ? "active" : "")
+                        }
+                      >
+                        <ChatIcon />
+                      </IconButton>
+                    </Box>
+                    {post.images &&
+                      post.images.map((image, index) => (
+                        <Box key={index} sx={{ mt: 1 }}>
+                          <img
+                            src={image}
+                            alt="Sample"
+                            style={{
+                              width: "150px",
+                              height: "150px",
+                              objectFit: "cover",
+                              borderRadius: "8px",
+                            }}
+                          />
+                        </Box>
+                      ))}
+                    {showTextSection[post._id] && (
+                      <CommentTextsection
+                        setShowThis={() => handleToggleTextSection(post._id)}
+                      />
+                    )}
+                  </div>
+                </Fade>
+              ) : null // Render nothing if post or post.author is missing
+          )}
+        <Typography variant="body2">No more Posts to show</Typography>
+      </div>
+    </div>
+  );
 };
 
 export default AllPost;
