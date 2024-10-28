f2e78b0e919eb8eb443e8a4ad04addf6d0e2068f
VBsaini <vbsaini2004@gmail.com>
2024-10-29 01:24:22 +0530

    UI bug fix - fix white shade on first post, moved the like, dislike and text button below the images and use module.css

--- frontend/src/User/myPosts.js
+++ frontend/src/User/myPosts.js
@@ -8,10 +8,11 @@ import {
 } from "@mui/icons-material";
 import DropdownMenu from "../shared/components/HamburgerDropdown";
 import { fetchMyPosts } from "../store/slices/postSlice";
-import "../styles/posts.css";
+import styles from "../styles/post.module.css";
 import CreatePost from "./createPost";
 import CommentTextsection from "../shared/components/CommentTextSection";
 import Error from "../shared/components/Error";
+import { Fade } from "react-awesome-reveal";
 
 const MyPost = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -23,6 +24,8 @@ const MyPost = () => {
   const [likes, setLikes] = useState({});
   const [likeMode, setLikeMode] = useState({});
 
+  const [postLikeList, setPostLikeList] = useState({});
+  const [isCommentActive, setIsCommentActive] = useState({});
   useEffect(() => {
     dispatch(fetchMyPosts());
   }, [dispatch]);
@@ -32,24 +35,35 @@ const MyPost = () => {
   };
 
   const handleToggleTextSection = (postId) => {
+    setIsCommentActive((prevState) => ({
+      ...prevState,
+      [postId]: !prevState[postId],
+    }));
     setShowTextSection((prevState) => ({
       ...prevState,
       [postId]: !prevState[postId],
     }));
   };
 
-  const handleToggleLike = (postId) => {
+  const handleToggleLike = (postId, like = 0) => {
     setLikes((prevState) => {
       const newLikes = { ...prevState };
       const isLiked = likeMode[postId];
-
       if (isLiked) {
         newLikes[postId] = Math.max(0, (newLikes[postId] || 0) - 1);
       } else {
         newLikes[postId] = (newLikes[postId] || 0) + 1;
       }
-
       return newLikes;
+    });
+    setPostLikeList((prevState) => {
+      const PostLikes = { ...prevState };
+      if (PostLikes[postId] == like) {
+        PostLikes[postId] = 0;
+      } else {
+        PostLikes[postId] = like;
+      }
+      return PostLikes;
     });
     setLikeMode((prevState) => ({
       ...prevState,
@@ -58,7 +72,7 @@ const MyPost = () => {
   };
 
   return (
-    <div className="posts-page">
+    <div className={styles.posts_page}>
       <Button
         variant="contained"
         color="primary"
@@ -70,60 +84,150 @@ const MyPost = () => {
       <div className="posts">
         {myPosts && myPosts.length > 0 ? (
           myPosts.map((post) => (
-            <div key={post._id} className="post">
-              <div className="post-owner">
-                <Avatar
-                  src={post.author.avatar.filePath}
-                  alt={post.author.name}
-                  sx={{ width: 56, height: 56 }}
-                />
-                <div className="username">
-                  <Typography variant="subtitle1">
-                    {post.author.name}
+            // <div key={post._id} className={styles.post}>
+            //   <div className={styles.post_owner}>
+            //     <Avatar
+            //       src={post.author.avatar.filePath}
+            //       alt={post.author.name}
+            //       sx={{ width: 56, height: 56 }}
+            //     />
+            //     <div className={styles.username}>
+            //       <Typography variant="subtitle1">
+            //         {post.author.name}
+            //       </Typography>
+            //     </div>
+            //     <DropdownMenu postId={post._id} userEmail={post.author.email} />
+            //   </div>
+            //   <div className="post-content">
+            //     <Typography variant="body1">{post.text}</Typography>
+            //   </div>
+            //   <Box sx={{ display: "flex", alignItems: "center", mt: 1 }}>
+            //     <IconButton onClick={() => handleToggleLike(post._id)}>
+            //       <ThumbUpIcon
+            //         color={likeMode[post._id] ? "primary" : "action"}
+            //       />
+            //     </IconButton>
+            //     <Typography variant="body2" sx={{ ml: 1 }}>
+            //       {likes[post._id] || 0}
+            //     </Typography>
+            //     <IconButton onClick={() => handleToggleLike(post._id)}>
+            //       <ThumbDownIcon />
+            //     </IconButton>
+            //     <IconButton onClick={() => handleToggleTextSection(post._id)}>
+            //       <ChatIcon />
+            //     </IconButton>
+            //   </Box>
+            //   {post.images &&
+            //     post.images.map((image, index) => (
+            //       <Box key={index} sx={{ mt: 1 }}>
+            //         <img
+            //           src={image}
+            //           alt="Sample"
+            //           style={{
+            //             width: "150px",
+            //             height: "150px",
+            //             objectFit: "cover",
+            //             borderRadius: "8px",
+            //           }}
+            //         />
+            //       </Box>
+            //     ))}
+            //   {showTextSection[post._id] && (
+            //     <CommentTextsection
+            //       setShowThis={() => handleToggleTextSection(post._id)}
+            //     />
+            //   )}
+            // </div>
+            <Fade cascade up fraction={0.2} triggerOnce>
+              <div key={post._id} className={styles.post}>
+                <div className={styles.post_owner}>
+                  <Avatar
+                    src={post.author.avatar.filePath}
+                    alt={post.author.name}
+                    sx={{ width: 56, height: 56 }} // Make sure this is consistent with CSS
+                  />
+                  <div className={styles.username}>
+                    <Typography
+                      variant="subtitle1"
+                      className={styles.post_owner_username}
+                    >
+                      {post.author.name}
+                    </Typography>
+                  </div>
+                  <DropdownMenu
+                    postId={post._id}
+                    userEmail={post.author.email}
+                  />
+                </div>
+                <div className={styles.post_content}>
+                  <Typography variant="body1" className={styles.post_text}>
+                    {post.text}
                   </Typography>
                 </div>
-                <DropdownMenu postId={post._id} userEmail={post.author.email} />
-              </div>
-              <div className="post-content">
-                <Typography variant="body1">{post.text}</Typography>
-              </div>
-              <Box sx={{ display: "flex", alignItems: "center", mt: 1 }}>
-                <IconButton onClick={() => handleToggleLike(post._id)}>
-                  <ThumbUpIcon
-                    color={likeMode[post._id] ? "primary" : "action"}
+                {post.images &&
+                  post.images.map((image, index) => (
+                    <Box key={index} sx={{ mt: 1 }}>
+                      <img
+                        src={image}
+                        alt="Sample"
+                        style={{
+                          width: "150px",
+                          height: "150px",
+                          objectFit: "cover",
+                          borderRadius: "8px",
+                        }}
+                      />
+                    </Box>
+                  ))}
+                <Box
+                  sx={{ display: "flex", alignItems: "center", mt: 1 }}
+                  className={styles.post_actions}
+                >
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      ml: 1,
+                      mt: 1,
+                      mr: 1,
+                      fontSize: 16,
+                      color: "rgb(198, 198, 198)",
+                    }}
+                  >
+                    {likes[post._id] || 0}
+                  </Typography>
+                  <IconButton
+                    onClick={(e) => handleToggleLike(post._id, 1)}
+                    className={`${styles.like_button} ${
+                      postLikeList[post._id] == 1 ? styles.active : ""
+                    }`}
+                  >
+                    <ThumbUpIcon />
+                  </IconButton>
+
+                  <IconButton
+                    onClick={() => handleToggleLike(post._id, -1)}
+                    className={`${styles.dislike_button} ${
+                      postLikeList[post._id] == -1 ? styles.active : ""
+                    }`}
+                  >
+                    <ThumbDownIcon />
+                  </IconButton>
+                  <IconButton
+                    onClick={() => handleToggleTextSection(post._id)}
+                    className={`${styles.comment_button} ${
+                      isCommentActive[post._id] == 1 ? styles.active : ""
+                    }`}
+                  >
+                    <ChatIcon />
+                  </IconButton>
+                </Box>
+                {showTextSection[post._id] && (
+                  <CommentTextsection
+                    setShowThis={() => handleToggleTextSection(post._id)}
                   />
-                </IconButton>
-                <Typography variant="body2" sx={{ ml: 1 }}>
-                  {likes[post._id] || 0}
-                </Typography>
-                <IconButton onClick={() => handleToggleLike(post._id)}>
-                  <ThumbDownIcon />
-                </IconButton>
-                <IconButton onClick={() => handleToggleTextSection(post._id)}>
-                  <ChatIcon />
-                </IconButton>
-              </Box>
-              {post.images &&
-                post.images.map((image, index) => (
-                  <Box key={index} sx={{ mt: 1 }}>
-                    <img
-                      src={image}
-                      alt="Sample"
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        objectFit: "cover",
-                        borderRadius: "8px",
-                      }}
-                    />
-                  </Box>
-                ))}
-              {showTextSection[post._id] && (
-                <CommentTextsection
-                  setShowThis={() => handleToggleTextSection(post._id)}
-                />
-              )}
-            </div>
+                )}
+              </div>
+            </Fade>
           ))
         ) : (
           <Typography variant="body2" sx={{ mt: 2 }}>
--- frontend/src/shared/components/AllPosts.js
+++ frontend/src/shared/components/AllPosts.js
@@ -8,7 +8,7 @@ import {
 } from "@mui/icons-material";
 import DropdownMenu from "./HamburgerDropdown";
 import { fetchAllPosts } from "../../store/slices/postSlice";
-import "../../styles/posts.css";
+import styles from "../../styles/post.module.css";
 import CommentTextsection from "./CommentTextSection";
 
 import { Fade } from "react-awesome-reveal";
@@ -21,13 +21,16 @@ const AllPost = () => {
   const [likeMode, setLikeMode] = useState({});
   //   to Hold likes and dislikes for a post
   const [postLikeList, setPostLikeList] = useState({});
-  const [isCommentActive, setIsCommentActive] = useState(false);
+  const [isCommentActive, setIsCommentActive] = useState({});
   useEffect(() => {
     dispatch(fetchAllPosts());
   }, [dispatch]);
 
   const handleToggleTextSection = (postId) => {
-    setIsCommentActive(!isCommentActive);
+    setIsCommentActive((prevState) => ({
+      ...prevState,
+      [postId]: !prevState[postId],
+    }));
     setShowTextSection((prevState) => ({
       ...prevState,
       [postId]: !prevState[postId],
@@ -63,24 +66,24 @@ const AllPost = () => {
   const { userInfo } = useSelector((state) => state.auth);
 
   return (
-    <div className="posts-page">
-      <div className="posts">
+    <div className={styles.posts_page}>
+      <div className={styles.posts}>
         {allPosts &&
           allPosts.map(
             (post) =>
               post && post.author ? ( // Check if post and post.author exist
                 <Fade cascade up fraction={0.2} triggerOnce>
-                  <div key={post._id} className="post">
-                    <div className="post-owner">
+                  <div key={post._id} className={styles.post}>
+                    <div className={styles.post_owner}>
                       <Avatar
                         src={post.author.avatar.filePath}
                         alt={post.author.name}
                         sx={{ width: 56, height: 56 }} // Make sure this is consistent with CSS
                       />
-                      <div className="username">
+                      <div className={styles.username}>
                         <Typography
                           variant="subtitle1"
-                          className="post-owner-username"
+                          className={styles.post_owner_username}
                         >
                           {post.author.name}
                         </Typography>
@@ -90,55 +93,11 @@ const AllPost = () => {
                         userEmail={post.author.email}
                       />
                     </div>
-                    <div className="post-content">
-                      <Typography variant="body1" className="post-text">
+                    <div className={styles.post_content}>
+                      <Typography variant="body1" className={styles.post_text}>
                         {post.text}
                       </Typography>
                     </div>
-                    <Box
-                      sx={{ display: "flex", alignItems: "center", mt: 1 }}
-                      className="post-actions"
-                    >
-                      <Typography
-                        variant="body2"
-                        sx={{
-                          ml: 1,
-                          mt: 1,
-                          mr: 1,
-                          fontSize: 16,
-                          color: "rgb(198, 198, 198)",
-                        }}
-                      >
-                        {likes[post._id] || 0}
-                      </Typography>
-                      <IconButton
-                        onClick={(e) => handleToggleLike(post._id, 1)}
-                        className={
-                          "like-button " +
-                          (postLikeList[post._id] == 1 ? "active" : "")
-                        }
-                      >
-                        <ThumbUpIcon />
-                      </IconButton>
-
-                      <IconButton
-                        onClick={() => handleToggleLike(post._id, -1)}
-                        className={
-                          "dislike-button " +
-                          (postLikeList[post._id] == -1 ? "active" : "")
-                        }
-                      >
-                        <ThumbDownIcon />
-                      </IconButton>
-                      <IconButton
-                        onClick={() => handleToggleTextSection(post._id)}
-                        className={
-                          "comment-button " + (isCommentActive ? "active" : "")
-                        }
-                      >
-                        <ChatIcon />
-                      </IconButton>
-                    </Box>
                     {post.images &&
                       post.images.map((image, index) => (
                         <Box key={index} sx={{ mt: 1 }}>
@@ -154,6 +113,48 @@ const AllPost = () => {
                           />
                         </Box>
                       ))}
+                    <Box
+                      sx={{ display: "flex", alignItems: "center", mt: 1 }}
+                      className={styles.post_actions}
+                    >
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          ml: 1,
+                          mt: 1,
+                          mr: 1,
+                          fontSize: 16,
+                          color: "rgb(198, 198, 198)",
+                        }}
+                      >
+                        {likes[post._id] || 0}
+                      </Typography>
+                      <IconButton
+                        onClick={(e) => handleToggleLike(post._id, 1)}
+                        className={`${styles.like_button} ${
+                          postLikeList[post._id] == 1 ? styles.active : ""
+                        }`}
+                      >
+                        <ThumbUpIcon />
+                      </IconButton>
+
+                      <IconButton
+                        onClick={() => handleToggleLike(post._id, -1)}
+                        className={`${styles.dislike_button} ${
+                          postLikeList[post._id] == -1 ? styles.active : ""
+                        }`}
+                      >
+                        <ThumbDownIcon />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => handleToggleTextSection(post._id)}
+                        className={`${styles.comment_button} ${
+                          isCommentActive[post._id] == 1 ? styles.active : ""
+                        }`}
+                      >
+                        <ChatIcon />
+                      </IconButton>
+                    </Box>
                     {showTextSection[post._id] && (
                       <CommentTextsection
                         setShowThis={() => handleToggleTextSection(post._id)}
@@ -163,7 +164,18 @@ const AllPost = () => {
                 </Fade>
               ) : null // Render nothing if post or post.author is missing
           )}
-        <Typography variant="body2">No more Posts to show</Typography>
+        <Typography
+          variant="body2"
+          style={{
+            margin: "0px 0px 0 20px",
+            padding: "20px",
+            color: "white",
+            textAlign: "center",
+            fontSize: "18px",
+          }}
+        >
+          No more Posts to show
+        </Typography>
       </div>
     </div>
   );
