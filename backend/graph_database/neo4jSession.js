b62553233888c36e62cb2665e8a850c66118c985
Tarik Anowar <itarikju@gmail.com>
2024-12-28 19:01:16 +0530

    fixed graph and vector database queries

--- backend/graph_database/neo4jSession.js
+++ backend/graph_database/neo4jSession.js
@@ -5,7 +5,6 @@ USER_NAME = process.env.NEO4J_USER;
 PASSWORD = process.env.NEO4J_PASSWORD;
 NEO4J_URI = process.env.NEO4J_URI;
 const func = () =>{
-    console.log(USER_NAME, PASSWORD, NEO4J_URI);
     if(!USER_NAME||!PASSWORD||!NEO4J_URI){
         console.log('Please provide the required environment variables');
         process.exit(1);
